6e7e7cd6128f8d182fddc4710f6eb443e5ddffdf
Serhii Shyian <serhii.shn@gmail.com>
2025-04-09 12:18:05 +0300

    add basic structure

--- src/main/java/com/example/winehood/WineHoodApplication.java
+++ src/main/java/com/example/winehood/WineHoodApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WinehoodApplication {
+public class WineHoodApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WinehoodApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(WineHoodApplication.class, args);
+    }
 
 }
