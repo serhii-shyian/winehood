ea3e062786c948daa806261dec0668eb86667b51
Serhii Shyian <serhii.shn@gmail.com>
2025-04-09 11:50:45 +0300

    initial commit

---
+++ src/test/java/com/example/winehood/WinehoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.winehood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WinehoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
